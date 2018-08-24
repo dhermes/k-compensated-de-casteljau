8f0b76ab6d43e6848c86f5728414a50dcd5efa8b
Danny Hermes <daniel.j.hermes@gmail.com>
2018-08-23 21:12:22 -0700

    Adding `basic` implementation in C++ and adding namespaces.

--- src/de_casteljau.cpp
+++ src/de_casteljau.cpp
@@ -1,5 +1,8 @@
 #include <cmath>
 #include <tuple>
+#include <vector>
+
+namespace eft {
 
 std::tuple<double, double> two_prod(double val1, double val2)
 {
@@ -16,4 +19,25 @@ std::tuple<double, double> two_sum(double val1, double val2)
     almost_val2 = sum - val1;
     error = (val1 - (sum - almost_val2)) + (val2 - almost_val2);
     return std::make_tuple(sum, error);
+}
+}
+
+namespace de_casteljau {
+
+double basic(double s, std::vector<double> coeffs)
+{
+    size_t degree, j, k;
+    double r;
+    std::vector<double> pk(coeffs);
+
+    r = 1.0 - s;
+    degree = coeffs.size() - 1;
+    for (k = 0; k < degree; ++k) {
+        for (j = 0; j < degree - k; ++j) {
+            pk[j] = r * pk[j] + s * pk[j + 1];
+        }
+    }
+
+    return pk[0];
+}
 }
