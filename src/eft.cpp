b11590471d8f8196f545bc6a9a990481310ec8bc
Danny Hermes <daniel.j.hermes@gmail.com>
2018-08-25 14:33:37 -0700

    Removing all uninitialized variables in `eft.cpp`.

--- src/eft.cpp
+++ src/eft.cpp
@@ -19,43 +19,35 @@ namespace eft {
 
 std::pair<double, double> two_prod(double val1, double val2)
 {
-    double product, error;
-    product = val1 * val2;
-    error = std::fma(val1, val2, -product);
+    double product = val1 * val2;
+    double error = std::fma(val1, val2, -product);
     return std::make_pair(product, error);
 }
 
 std::pair<double, double> two_sum(double val1, double val2)
 {
-    double sum, almost_val2, error;
-    sum = val1 + val2;
-    almost_val2 = sum - val1;
-    error = (val1 - (sum - almost_val2)) + (val2 - almost_val2);
+    double sum = val1 + val2;
+    double almost_val2 = sum - val1;
+    double error = (val1 - (sum - almost_val2)) + (val2 - almost_val2);
     return std::make_pair(sum, error);
 }
 
 void vec_sum(std::vector<double>& vec)
 {
-    size_t n, i;
-
-    n = vec.size();
-    for (i = 1; i < n; ++i) {
+    for (size_t i = 1; i < vec.size(); ++i) {
         std::tie(vec[i], vec[i - 1]) = two_sum(vec[i], vec[i - 1]);
     }
 }
 
 double sum_k(const std::vector<double>& vec, size_t K)
 {
-    size_t i;
-    std::vector<double> workspace;
-    double result;
+    std::vector<double> workspace = vec;
 
-    workspace = vec;
-    for (i = 0; i < K - 1; ++i) {
+    for (size_t i = 0; i < K - 1; ++i) {
         vec_sum(workspace);
     }
 
-    result = 0.0;
+    double result = 0.0;
     for (double const& element : workspace) {
         result += element;
     }
