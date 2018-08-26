c210480c365bb01ee856bf9c1a80a027d5a3ee7e
Danny Hermes <daniel.j.hermes@gmail.com>
2018-08-26 14:55:36 -0700

    Refactoring `tests.cpp` to use the templated `compensated<K, degree>`.
    Also making all variables be assigned when allocated and moving
    to the most specific scope possible.

--- scripts/tests.cpp
+++ scripts/tests.cpp
@@ -12,6 +12,7 @@
 
 #include "de_casteljau.hpp"
 #include "eft.hpp"
+#include <array>
 #include <cmath>
 #include <iostream>
 #include <string>
@@ -19,13 +20,8 @@
 
 int main()
 {
-    size_t i, j, F, K;
-    double s, evaluated;
-    std::vector<double> compensated;
-    std::vector<std::vector<double>> coeffs;
-    std::pair<double, double> eft_result;
-
-    eft_result = eft::two_prod(1.0 - pow(0.5, 27), 1.0 + pow(0.5, 27));
+    std::pair<double, double> eft_result
+        = eft::two_prod(1.0 - pow(0.5, 27), 1.0 + pow(0.5, 27));
     std::cout << "two_prod(1 - 2^{-27}, 1 + 2^{-27}) = P + pi, where"
               << std::endl;
     std::cout << "P  = " << eft_result.first << std::endl;
@@ -41,43 +37,97 @@ int main()
               << std::endl;
 
     std::cout << std::string(60, '=') << std::endl;
-    coeffs.resize(3);
+    std::vector<std::vector<double>> coeffs(3);
     coeffs[0] = { 3.0, 2.0 };
+    std::array<double, 2> coeffs1 = { 3.0, 2.0 };
     std::cout << "p1(s) = 3(1 - s) + 2s" << std::endl;
     coeffs[1] = { 3.0, 2.5, 0.0 };
+    std::array<double, 3> coeffs2 = { 3.0, 2.5, 0.0 };
     std::cout << "p2(s) = 3(1 - s)^2 + 2.5[2(1 - s)s]" << std::endl;
     coeffs[2] = { 4.0, 0.0, 0.0, -1.0 };
+    std::array<double, 4> coeffs3 = { 4.0, 0.0, 0.0, -1.0 };
     std::cout << "p3(s) = 4(1 - s)^3 - s^3" << std::endl;
 
     std::cout << std::string(60, '=') << std::endl;
     std::cout << std::scientific;
     std::cout << "DeCasteljau:" << std::endl;
-    for (i = 0; i < 5; ++i) {
-        s = 1.5 * i - 3.0;
-        for (j = 0; j < 3; ++j) {
-            evaluated = de_casteljau::basic(s, coeffs[j]);
+    for (size_t i = 0; i < 5; ++i) {
+        double s = 1.5 * i - 3.0;
+        for (size_t j = 0; j < 3; ++j) {
+            double evaluated = de_casteljau::basic(s, coeffs[j]);
             std::cout << "p" << j + 1 << "(" << s << ") = " << evaluated
                       << std::endl;
         }
         std::cout << std::string(30, '*') << std::endl;
     }
 
-    for (K = 2; K <= 4; ++K) {
-        std::cout << std::string(60, '=') << std::endl;
-        std::cout << "CompDeCasteljau" << K << ":" << std::endl;
-        for (i = 0; i < 5; ++i) {
-            s = 1.5 * i - 3.0 + pow(0.5, 50);
-            for (j = 0; j < 3; ++j) {
-                compensated = de_casteljau::compensated_k(s, coeffs[j], K);
-                std::cout << "p" << j + 1 << "(" << s
-                          << ") = " << compensated[0];
-                for (F = 1; F < K; ++F) {
-                    std::cout << " + (" << compensated[F] << ")";
-                }
-                std::cout << std::endl;
-            }
-            std::cout << std::string(30, '*') << std::endl;
-        }
+    std::cout << std::string(60, '=') << std::endl;
+    std::cout << "CompDeCasteljau:" << std::endl;
+    for (size_t i = 0; i < 5; ++i) {
+        double s = 1.5 * i - 3.0 + pow(0.5, 50);
+
+        std::array<double, 2> compensated2
+            = de_casteljau::compensated<2, 1>(s, coeffs1);
+        std::cout << "p1(" << s << ") = " << compensated2[0] << " + ("
+                  << compensated2[1] << ")" << std::endl;
+
+        compensated2 = de_casteljau::compensated<2, 2>(s, coeffs2);
+        std::cout << "p2(" << s << ") = " << compensated2[0] << " + ("
+                  << compensated2[1] << ")" << std::endl;
+
+        compensated2 = de_casteljau::compensated<2, 3>(s, coeffs3);
+        std::cout << "p3(" << s << ") = " << compensated2[0] << " + ("
+                  << compensated2[1] << ")" << std::endl;
+
+        std::cout << std::string(30, '*') << std::endl;
+    }
+
+    std::cout << std::string(60, '=') << std::endl;
+    std::cout << "CompDeCasteljau3:" << std::endl;
+    for (size_t i = 0; i < 5; ++i) {
+        double s = 1.5 * i - 3.0 + pow(0.5, 50);
+
+        std::array<double, 3> compensated3
+            = de_casteljau::compensated<3, 1>(s, coeffs1);
+        std::cout << "p1(" << s << ") = " << compensated3[0] << " + ("
+                  << compensated3[1] << ") + (" << compensated3[2] << ")"
+                  << std::endl;
+
+        compensated3 = de_casteljau::compensated<3, 2>(s, coeffs2);
+        std::cout << "p2(" << s << ") = " << compensated3[0] << " + ("
+                  << compensated3[1] << ") + (" << compensated3[2] << ")"
+                  << std::endl;
+
+        compensated3 = de_casteljau::compensated<3, 3>(s, coeffs3);
+        std::cout << "p3(" << s << ") = " << compensated3[0] << " + ("
+                  << compensated3[1] << ") + (" << compensated3[2] << ")"
+                  << std::endl;
+
+        std::cout << std::string(30, '*') << std::endl;
+    }
+
+    std::cout << std::string(60, '=') << std::endl;
+    std::cout << "CompDeCasteljau4:" << std::endl;
+    for (size_t i = 0; i < 5; ++i) {
+        double s = 1.5 * i - 3.0 + pow(0.5, 50);
+
+        std::array<double, 4> compensated4
+            = de_casteljau::compensated<4, 1>(s, coeffs1);
+        std::cout << "p1(" << s << ") = " << compensated4[0] << " + ("
+                  << compensated4[1] << ") + (" << compensated4[2] << ") + ("
+                  << compensated4[3] << ")" << std::endl;
+
+        compensated4 = de_casteljau::compensated<4, 2>(s, coeffs2);
+        std::cout << "p2(" << s << ") = " << compensated4[0] << " + ("
+                  << compensated4[1] << ") + (" << compensated4[2] << ") + ("
+                  << compensated4[3] << ")" << std::endl;
+
+        compensated4 = de_casteljau::compensated<4, 3>(s, coeffs3);
+        std::cout << "p3(" << s << ") = " << compensated4[0] << " + ("
+                  << compensated4[1] << ") + (" << compensated4[2] << ") + ("
+                  << compensated4[3] << ")" << std::endl;
+
+        std::cout << std::string(30, '*') << std::endl;
     }
 
     return 0;
